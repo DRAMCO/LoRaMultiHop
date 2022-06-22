4c182c68807f9fb06bfaaf49810a42d22ca6ba5c
Geoffrey Ottoy <geoffrey.ottoy@kuleuven.be>
2022-06-22 15:51:50 +0200

    cleanup

--- Logger/main.py
+++ Logger/main.py
@@ -9,14 +9,15 @@ from datetime import datetime
 # general settings
 port = 'COM31'
 
-MESG_FIRST_BYTE_OFFSET = 1
+MESG_UID_SIZE = 2
+NODE_UID_SIZE = 1
+
+MESG_FIRST_BYTE_OFFSET = 1      # start of the actual message "Packet:" is position 0
 MESG_UID_OFFSET = 0
-TYPE_OFFSET = 2
-HOPS_OFFSET = 3
-NEXT_PREVIOUS_UID_OFFSET = 4
-NODE_UID_OFFSET = 6
-PAYLOAD_SIZE_OFFSET = 8
-PAYLOAD_OFFSET = 9
+TYPE_OFFSET = MESG_UID_OFFSET + MESG_UID_SIZE
+HOPS_OFFSET = TYPE_OFFSET + 1
+NEXT_PREVIOUS_UID_OFFSET = HOPS_OFFSET + 1
+PAYLOAD_OFFSET = NEXT_PREVIOUS_UID_OFFSET + NODE_UID_SIZE
 
 
 # parse a line
@@ -24,7 +25,10 @@ def parse_line(line_str):
     parts = line_str.rstrip().split(' ')
     # read messageUid
     offset = MESG_FIRST_BYTE_OFFSET + MESG_UID_OFFSET
-    message_uid = parts[offset] + parts[offset + 1]
+    if NODE_UID_SIZE == 2:
+        message_uid = parts[offset] + parts[offset + 1]
+    else:
+        message_uid = parts[offset]
     # read messageType
     offset = MESG_FIRST_BYTE_OFFSET + TYPE_OFFSET
     message_type = parts[offset]
@@ -33,37 +37,13 @@ def parse_line(line_str):
     hops = parts[offset]
     # read nextPreviousUid
     offset = MESG_FIRST_BYTE_OFFSET + NEXT_PREVIOUS_UID_OFFSET
-    destination_uid = parts[offset] + parts[offset + 1]  # should always be 0000
-    # read sourceUid
-    offset = MESG_FIRST_BYTE_OFFSET + NODE_UID_OFFSET
-    source_uid = parts[offset] + parts[offset + 1]
-    # read payloadLength
-    offset = MESG_FIRST_BYTE_OFFSET + PAYLOAD_SIZE_OFFSET
-    payload_length = parts[offset]
-    try:
-        payload_length_int = int(payload_length, 16)
-        offset = MESG_FIRST_BYTE_OFFSET + PAYLOAD_OFFSET
-        payload_data = parts[offset:(offset + payload_length_int)]
-        rest_of_line = parts[(offset + payload_length_int):]
-    except ValueError:
-        print("Wrong payload length: " + payload_length)
-        payload_data = []
-        rest_of_line = []
-        payload_length_int = -1
-
-    # dictionary to store all payloads from this message
-    # we initialize using the first payload
-    payload = {
-        "nr_of_payloads": 1,
-        "payloads": []
-    }
-    first_payload = {
-        "source_uid": source_uid,
-        "payload_length": payload_length_int,
-        "payload_data": payload_data
-    }
-
-    payload["payloads"].append(first_payload)
+    if NODE_UID_SIZE == 2:
+        destination_uid = parts[offset] + parts[offset + 1]  # should always be 0000
+    else:
+        destination_uid = parts[offset]
+    # read payload
+    offset = MESG_FIRST_BYTE_OFFSET + PAYLOAD_OFFSET
+    payload = parse_payload(parts[offset:])
 
     # general info for this message
     message_info = {
@@ -72,50 +52,68 @@ def parse_line(line_str):
         "hops": hops,
         "destination_uid": destination_uid,
         "payload": payload,
-        "timestamp": datetime.now().strftime("%Y%m%d-%H%M%S")
+        "timestamp": datetime.now().strftime("%Y%m%d-%H%M%S"),
+        "raw_message": ' '.join(parts[MESG_FIRST_BYTE_OFFSET:])
     }
-
-    # check if more payloads were appended
-    rest_parsed = True
-    if not len(rest_of_line) == 0:
-        rest_parsed = False
-    while not rest_parsed:
-        (new_payload, rest_of_line) = parse_rest(rest_of_line)
-        # add payload to payloads
-        payload = message_info["payload"]
-        payload["nr_of_payloads"] = payload["nr_of_payloads"] + 1
-        payload["payloads"].append(new_payload)
-        if len(rest_of_line) == 0:
-            rest_parsed = True
 
     return message_info
 
 
 # parse the rest of a line
-def parse_rest(line_parts):
-    if not len(line_parts) > 3:
+def parse_payload(line_parts):
+    if not len(line_parts) > 1:
         print("Error parsing appended payload (not enough fields remaining)")
         return {}, []
 
-    source_uid = line_parts[0] + line_parts[1]
-    payload_length = line_parts[2]
+    offset = 0
+    if NODE_UID_SIZE == 2:
+        source_uid = line_parts[0] + line_parts[1]
+    else:
+        source_uid = line_parts[0]
+
+    offset += NODE_UID_SIZE
     try:
-        payload_length_int = int(payload_length, 16)
-        payload_data = line_parts[3:(3 + payload_length_int)]
-        rest_of_line = line_parts[(3 + payload_length_int):]
+        length_field = int(line_parts[offset], 16)
     except ValueError:
-        print("Wrong payload length (in rest): " + payload_length)
-        payload_data = []
-        rest_of_line = []
-        payload_length_int = -1
+        print("ERROR: length field is not an integer -> ", line_parts[NODE_UID_SIZE])
 
-    payload = {
-        "source_uid": source_uid,
-        "payload_length": payload_length_int,
-        "payload_data": payload_data
-    }
+    # extract length of forwarded data (5 Msb) and own data (3 Lsb) from the length field
+    own_data_length = (length_field & 0x07)
+    forwarded_data_length = ((length_field >> 3) & 0x1F)
+    offset += 1
+    own_data = line_parts[offset:(offset+own_data_length)]
+    rest_of_data = line_parts[(offset+own_data_length):]
+    if forwarded_data_length == 0:
+        print("own data:")
+        print(own_data)
+        print("rest of data")
+        print(rest_of_data)
+        forwarded_data = []
+        forwarded_payload = []
+    else:
+        forwarded_data = line_parts[(offset+own_data_length):]
+        print("forwarded payload:")
+        print(forwarded_data)
+        forwarded_payload = parse_payload(forwarded_data)
+    # extra check
+    if not (len(forwarded_data) == forwarded_data_length):
+        print("ERROR: forwarded data length does not match length field")
 
-    return payload, rest_of_line
+    payload = [
+        {
+            "source_uid": source_uid,
+            "payload_length": own_data_length,
+            "payload_data": own_data,
+            "forwarded_payload": forwarded_payload
+        }
+    ]
+
+    if forwarded_data_length == 0 and len(rest_of_data) > 0:
+        el = parse_payload(rest_of_data)
+        for e in el:
+            payload.append(e)
+
+    return payload
 
 
 def run_logger(port_name):
@@ -149,6 +147,7 @@ def run_logger(port_name):
 
                     # parse line containing message to json format
                     message_info = parse_line(line)
+                    print(message_info)
 
                     # add message_info to list // TODO: do we have enough RAM?
                     message_log["nr_of_messages"] = message_log["nr_of_messages"] + 1
@@ -169,6 +168,18 @@ def run_logger(port_name):
     with io.open(filename, 'w', encoding='utf-8') as json_file:
         json_data = json.dumps(message_log, ensure_ascii=False, indent=4)
         json_file.write(json_data)
+
+
+def get_uids_from_payload(payload_data):
+    uid = [payload_data["source_uid"]]
+    fwp = payload_data["forwarded_payload"]
+    if len(fwp) == 0:
+        return uid
+    else:
+        uids = get_uids_from_payload(fwp)
+        for u in uids:
+            uid.append(u)
+        return uid
 
 
 def run_analysis(file_name):
@@ -200,17 +211,18 @@ def run_analysis(file_name):
             # first we look for all the different nodes we can find
             for message in data["messages"]:
                 payload = message["payload"]
-                if payload["nr_of_payloads"] > 0:
-                    for single_payload in payload["payloads"]:
-                        node_uid = single_payload["source_uid"]
-                        node_uid_in_list = False
-                        for node_info in node_statistics_unsorted:
-                            if len(analysed_data) > 0:
-                                if node_info["node_uid"] == node_uid:
-                                    node_uid_in_list = True
-                                    break
-                        if not node_uid_in_list:
-                            node_statistics_unsorted.append({"node_uid": node_uid})
+
+                # find all different occurring node uid's
+                node_uids = get_uids_from_payload(payload)
+                # for each node_uid found, check if it is allready in the statistics, otherwise add it to the list
+                for node_uid in node_uids:
+                    node_uid_in_list = False
+                    for node_info in node_statistics_unsorted:
+                        if node_info["node_uid"] == node_uid:
+                            node_uid_in_list = True
+                            break
+                    if not node_uid_in_list:
+                        node_statistics_unsorted.append({"node_uid": node_uid})
 
             node_statistics_sorted = sorted(node_statistics_unsorted, key=lambda k: k["node_uid"])
             analysed_data["node_statistics"] = node_statistics_sorted
@@ -225,29 +237,28 @@ def run_analysis(file_name):
 if __name__ == '__main__':
     valid_option = False
 
-    #run_analysis('')
-
-    while not valid_option:
-        ans = input("Analyze (A) or log (L): ")
-
-        if ans.upper() == 'A':
-            valid_option = True
-            print("Running analysis...")
-
-        if ans.upper() == 'L':
-            valid_option = True
-            print("Starting logger...")
-
-            prompt = "Specify port or enter for (" + port + "): "
-            ans = input(prompt)
-            if len(ans.rstrip()) == 0:
-                ans = port.upper()
-
-            run_logger(ans)
+    # while not valid_option:
+    #     ans = input("Analyze (A) or log (L): ")
+    #
+    #     if ans.upper() == 'A':
+    #         valid_option = True
+    #         run_analysis('')
+    #         print("Running analysis...")
+    #
+    #     if ans.upper() == 'L':
+    #         valid_option = True
+    #         print("Starting logger...")
+    #
+    #         prompt = "Specify port or enter for (" + port + "): "
+    #         ans = input(prompt)
+    #         if len(ans.rstrip()) == 0:
+    #             ans = port.upper()
+    #
+    #         run_logger(ans)
 
     # Test parsing of a packet
-    # line = "Packet: 45 7E 03 00 00 00 30 00 02 00 6B 20 00 01 55 12 34 03 10 20 FF\n"
-    # info = parse_line(line)
-    # print(str(info))
+    line = "Packet: F7 A0 03 00 00 10 62 00 50 50 02 00 1B 20 02 00 55 D0 02 00 09 "
+    info = parse_line(line)
+    print(str(info))
 
     print('program end')
