e0dd731315d077408c349c46521eba67120d2e8d
Guus Leenders <leenders.guus@gmail.com>
2023-01-16 11:13:16 +0100

    Update LoRaMultiHop.h

--- DRAMCO-UNO/lib/MultiHop/LoRaMultiHop.h
+++ DRAMCO-UNO/lib/MultiHop/LoRaMultiHop.h
@@ -20,7 +20,7 @@
 // #define AGGREGATION_TIMER_UPSTEP            10000
 // #define AGGREGATION_TIMER_DOWNSTEP          0
 
-#define AGGREGATION_TIMER_NOMINAL           960000UL// in ms
+#define AGGREGATION_TIMER_NOMINAL           MEASURE_INTERVAL/2+60000UL// in ms
 #define AGGREGATION_TIMER_MIN               0 // In ms
 #define AGGREGATION_TIMER_MAX               AGGREGATION_TIMER_NOMINAL+60000 // In ms
 #define AGGREGATION_TIMER_RANDOM            90000 // Random window around PRESET_MAX_LATENCY
